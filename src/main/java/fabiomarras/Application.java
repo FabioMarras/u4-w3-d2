f049bf5e48bd96499e52c80eab2c5b80f5b11c97
FabioMarras <fabio.marras@yahoo.com>
2023-10-17 15:17:37 +0200

    add refresh method

--- src/main/java/fabiomarras/Application.java
+++ src/main/java/fabiomarras/Application.java
@@ -22,17 +22,24 @@ public class Application {
         sd.save(festaFabio);*/
 
         //FIND per cercare tramite id
-        /*Evento findById = sd.findById(UUID.fromString("6834b710-1165-486f-9bd5-6b51b7b650d3"));
+        Evento findById = sd.findById(UUID.fromString("77d43ada-af37-4190-80e1-4de7f99a0209"));
         if (findById != null) {
             System.out.println(findById);
         } else {
             System.out.println("non ho trovato niente");
-        }*/
+        }
 
         //DELETE per carncellare tramite id
         //sd.findByIdAndDelete(UUID.fromString("6834b710-1165-486f-9bd5-6b51b7b650d3"));
 
-        
+        //REFRESH per ricarica il contenuto
+        Evento refresh1 = sd.findById(UUID.fromString("77d43ada-af37-4190-80e1-4de7f99a0209"));
+        if (refresh1 != null) {
+            refresh1.setTitolo("Nuovo Titolo dell'evento");
+            System.out.println(refresh1);
+            em.refresh(refresh1);
+            System.out.println(refresh1);
+        }
 
         } catch (Exception ex){
             System.err.println(ex.getMessage());
